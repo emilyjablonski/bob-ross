52b8cdedc88562256c5772024523fd61e67f1795
emilyjablonski <emijablonski@gmail.com>
2024-08-22 19:19:57 -0600

    feat: initial season rows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,84 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from "next/image"
+import styles from "./page.module.css"
+
+const episodesPerSeason: Record<number, number> = {
+  1: 13,
+  2: 13,
+  3: 13,
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  interface PaintingProps {
+    season: number
+    episode: number
+  }
+
+  const Painting = (props: PaintingProps) => {
+    return (
+      <Image
+        src={`/paintings/bobross_season_${props.season}_episode_${props.episode}.png`}
+        alt={`Painting - Season ${props.season} Episode ${props.episode}`}
+        width={250}
+        height={175}
+      />
+    )
+  }
+
+  type PaintingNumber = {
+    season: number
+    episode: number
+  }
+
+  interface PaintingRowProps {
+    images: PaintingNumber[]
+  }
+
+  const PaintingRow = (props: PaintingRowProps) => {
+    return (
+      <div className={styles.row}>
+        <div className={styles.grid}>
+          {props.images.map((image) => {
+            return <Painting season={image.season} episode={image.episode} />
+          })}
         </div>
       </div>
+    )
+  }
 
+  interface PaintingSeasonProps {
+    season: number
+  }
+
+  const PaintingSeason = (props: PaintingSeasonProps) => {
+    return [...Array(Math.ceil(episodesPerSeason[props.season] / 4))].map((_, i) => {
+      const imagesPerRow =
+        (i + 1) * 4 > episodesPerSeason[props.season]
+          ? 4 + (episodesPerSeason[props.season] - (i + 1) * 4)
+          : 4
+
+      const images = [...Array(imagesPerRow)].map((_, j) => {
+        return { season: props.season, episode: i * 4 + 1 + j }
+      })
+
+      return <PaintingRow images={images} />
+    })
+  }
+
+  interface SeasonsProps {
+    seasons: number
+  }
+
+  const Seasons = (props: SeasonsProps) => {
+    return [...Array(props.seasons)].map((_, i) => {
+      return <PaintingSeason season={i + 1} />
+    })
+  }
+
+  return (
+    <main className={styles.main}>
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <Seasons seasons={Object.keys(episodesPerSeason).length} />
       </div>
     </main>
-  );
+  )
 }
